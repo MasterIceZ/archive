f0203afe81e9140e21f979e0bc8e89cb6871eac1
Borworntat Dendumrongkul <64594596+MasterIceZ@users.noreply.github.com>
2021-12-30 09:34:25 +0700

    disjoined set union

---
+++ data-structure-snippets/DSU.cpp
@@ -0,0 +1,12 @@
+struct DSU{
+	vector<int> parent;
+	DSU(size_t n){
+		parent.resize(2 + n);
+		for(int i=1; i<=n; ++i){
+			parent[i] = i;
+		}
+	}
+	int find_root(int u){
+		return u == parent[u] ? u: parent[u] = find_root(parent[u]);
+	};
+}
