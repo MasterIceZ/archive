4c067677e2513a9d0f79252bf37d216dac682af3
Borworntat Dendumrongkul <64594596+MasterIceZ@users.noreply.github.com>
2022-02-10 14:22:44 +0700

    added template

---
+++ cpp-template/0.cc
@@ -0,0 +1,43 @@
+/*
+ * AUTHOR	: Hydrolyzed~
+ * SCHOOL	: RYW
+ * TASK		:
+ * ALGO		:
+ * DATE		:
+ * */
+
+#include <bits/stdc++.h>
+// @==== Libary ====@ //
+
+// @================@ //
+
+using namespace std;
+
+// @=== Debugger ===@ //
+#ifdef _DEBUG
+#include "debug.hpp"
+#else
+#define dbg(...) 0
+#endif
+// @================@ //
+
+using ll = long long;
+
+int a[] = {0, 1, 2, 31, 4, 25, 63};
+
+inline void solution(){
+
+	return ;
+}
+
+signed main(){
+	cin.tie(nullptr)->ios::sync_with_stdio(false);	
+	int q = 1;
+//	cin >> q;
+	while(q--){
+		solution();
+		cout << "\n" ;
+	}
+	return 0;
+}
+// https://github.com/MasterIceZ/archive/tree/main/cpp-template
