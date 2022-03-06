25c0bdf9070f00736d666bdf6cdbad3b880b89e3
Borworntat Dendumrongkul <64594596+MasterIceZ@users.noreply.github.com>
2022-03-06 11:36:37 +0700

    added some function

--- cpp-template/debug.hpp
+++ cpp-template/debug.hpp
@@ -4,8 +4,8 @@
 
 
 // Header Guard
-#ifndef ___SELF_DEBUG___
-#define ___SELF_DEBUG___
+#ifndef ___SELF_DEBUG_TEMPLATE___
+#define ___SELF_DEBUG_TEMPLATE___
 
 // Include
 #include <iostream>
@@ -22,6 +22,13 @@ namespace UTILS{
 			 vt.push_back(a[i]);
 		 }
 		 return vt;
+	}
+	template<size_t S> vector<bool> to_vector(bitset<S> bs, size_t l, size_t r){
+		vector<bool> vt;
+		for(size_t i=l; i<=r; ++i){
+			vt.push_back(bs[i]);
+		}
+		return vt;
 	}
 }
 
@@ -41,6 +48,24 @@ string to_string(const char* s){
 	return string(s);
 }
 
+template<class T> string to_string(vector<T> vt, string sep=", "){
+	bool ok = false;
+	string res = "(";
+	for(auto x: vt){
+		if(ok){
+			res += sep;
+		}
+		ok = true;
+		res += to_string(x);
+	}
+	return res + ")";
+}
+
+template<size_t S> string to_string(bitset<S> bs, size_t l, size_t r, string sep=", "){
+	vector<bool> vt = UTILS::to_vector(bs, l, r);
+	return to_string(vt, sep);
+}
+
 template<class T, class S> string to_string(pair<T, S> p){
 	string res = "(";
 	res += to_string(p.first);
@@ -52,29 +77,34 @@ template<class T, class S> string to_string(pair<T, S> p){
 
 template<class T> string to_string(T a[], size_t l, size_t r, string sep=", "){
 	vector<T> vt = UTILS::to_vector(a, l, r);
-	string res = "(";
-	bool ok = false;
-	for(auto x: vt){
-		if(ok){
-			res += sep;
-		}
-		ok = true;
-		res += to_string(x);
-	}
-	return res + ")";
+	return to_string(vt, sep);
 }
 
-template<class T> string to_string(T v){
-	bool f = true;
-	string res;
-	for(auto x: v){
-		if(!f){
-			res += " ";
-		}
-		f = false;
-		res += to_string(x);
+template<class T> string to_string(stack<T> st, string sep=", "){
+	vector<T> vt;
+	while(!st.empty()){
+		vt.emplace_back((T)st.top());
+		st.pop();
 	}
-	return res;
+	return to_string(vt, sep);
+}
+
+template<class T> string to_string(queue<T> q, string sep=", "){
+	vector<T> vt;
+	while(!q.empty()){
+		vt.emplace_back((T)q.front());
+		q.pop();
+	}
+	return to_string(vt, sep);
+}
+
+template<class T> string to_string(priority_queue<T> pq, string sep=", "){
+	vector<T> vt;
+	while(!pq.empty()){
+		vt.emplace_back((T)pq.top());
+		pq.pop();
+	}
+	return to_string(vt, sep);
 }
 
 void DBG(){
