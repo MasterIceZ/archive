c5ab35043331d4e7e2a63c8cb2cd32b7701dabd6
Borworntat Dendumrongkul <64594596+MasterIceZ@users.noreply.github.com>
2022-08-27 18:33:21 +0700

    feat: added "latest" fenwick tree version :smiley:

---
+++ libs/fenwick_tree.cc
@@ -0,0 +1,29 @@
+template<typename T>
+class fenwick_tree{
+protected:
+	unsigned int offset = 1024;
+private:
+	unsigned int sz;
+	T *tree;
+public:
+	fenwick_tree(unsigned int _sz){
+		sz = _sz + offset;
+		tree = (T*) calloc(sz + offset, sizeof(T));
+		assert(tree);
+	}
+	inline T read(unsigned int idx){
+		T res = 0;
+		for(; idx; idx-=idx&-idx){
+			res = (res + tree[idx]);
+		}
+		return res;
+	}
+	inline void update(unsigned int idx, T value){
+		for(; idx<=sz; idx+=idx&-idx){
+			tree[idx] = (tree[idx] + value);
+		}
+	}
+	T operator [] (const unsigned int idx){
+		return read(idx);
+	}
+};
